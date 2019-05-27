d9f3c059b1b59a9d0cdc41e6daf505d9f0c59360
Matt Travi <programmer@travi.org>
2019-05-26 23:15:03 -0500

    feat(extraneous-deps): allowed dev-deps in test files

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = {
 
   rules: {
     'mocha/no-skipped-tests': 'error',
-    'filenames/match-regex': ['error', '^[a-z0-9\\-\\.]+-test$']
+    'filenames/match-regex': ['error', '^[a-z0-9\\-\\.]+-test$'],
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}]
   }
 };

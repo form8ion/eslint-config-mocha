dc834ce4f3d45d82e100f2ac15ca466556aed6a5
Matt Travi <programmer@travi.org>
2020-01-26 01:17:43 -0600

    fix(warning): disabled warning for setup/teardown for single test

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ module.exports = {
       'mocha/no-skipped-tests': 'error',
       'mocha/no-mocha-arrows': 'off',
       'mocha/no-setup-in-describe': 'off',
+      'mocha/no-hooks-for-single-case': 'off',
       'filenames/match-regex': ['error', '^[a-z0-9\\-\\.]+-test$'],
       'import/no-extraneous-dependencies': ['error', {devDependencies: true}]
     }
